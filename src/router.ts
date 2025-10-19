690245c422f41ad86ebc1aea477a5ff70688968b
joaquinabenza <jjabenza81@gmail.com>
2025-10-19 23:42:37 +0200

    fix paths

--- src/router.ts
+++ src/router.ts
@@ -3,10 +3,10 @@ import { createMemoryHistory, createRouter, RouteRecordRaw} from 'vue-router'
 const routes: RouteRecordRaw[] = [
   { path: '/:pathMatch(.*)*', redirect: '/'},
   { path: '/home', redirect: '/' },
-  { path: '/', component: () => import('./Views/Home.vue') },
-  { path: '/zoom', component: () => import('./Views/Zoom.vue') },
-  { path: '/Cards', component: () => import('./Views/Cards.vue') },
-  { path: '/parallax-controller', component: () => import('./Views/ParallaxController.vue') }
+  { path: '/', component: () => import('./views/Home.vue') },
+  { path: '/zoom', component: () => import('./views/Zoom.vue') },
+  { path: '/Cards', component: () => import('./views/Cards.vue') },
+  { path: '/parallax-controller', component: () => import('./views/ParallaxController.vue') }
 ]
 
 export const router = createRouter({
