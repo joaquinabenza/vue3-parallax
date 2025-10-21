41b75202b6137d4597d2086b41933136e30a6f09
joaquinabenza <jjabenza81@gmail.com>
2025-10-21 14:53:19 +0200

    fix images path

--- src/views/Background.vue
+++ src/views/Background.vue
@@ -2,7 +2,7 @@
   <div class="page-full">
     <h1 class="title">Parallax Background Examples</h1>
     <h2>Basic Usage</h2>
-    <ParallaxBackground :backgroundImage="'/723-1920x1080.jpg'" :speed="2" direction="down"
+    <ParallaxBackground :backgroundImage="'./723-1920x1080.jpg'" :speed="2" direction="down"
       backgroundPosition="top">
       <div class="hero-content">
         <h1>Welcome to My Site</h1>
@@ -10,7 +10,7 @@
       </div>
     </ParallaxBackground>
     <h2>Advanced Usage</h2>
-    <ParallaxBackground :backgroundImage="'/615-1920x1080.jpg'" :speed="3" direction="up"
+    <ParallaxBackground :backgroundImage="'./615-1920x1080.jpg'" :speed="3" direction="up"
       background-size="cover" :min-offset="-200" :max-offset="200">
       <div class="hero-content">
         <h1>Advanced Parallax</h1>
@@ -18,7 +18,7 @@
       </div>
     </ParallaxBackground>
     <h3>Different direction</h3>
-    <ParallaxBackground :backgroundImage="'/645-1920x1080.jpg'" :speed="3" direction="down"
+    <ParallaxBackground :backgroundImage="'./645-1920x1080.jpg'" :speed="3" direction="down"
       background-size="cover" :min-offset="-200" :max-offset="200">
       <div class="hero-content">
         <h1>Advanced Parallax</h1>
